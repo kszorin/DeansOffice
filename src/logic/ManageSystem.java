e417a21a3c3c6325ca6bf6f4138d58884d3a9664
kszorin <kszorin@yandex.ru>
2017-01-23 17:21:33 +0700

    something

--- src/logic/ManageSystem.java
+++ src/logic/ManageSystem.java
@@ -1,8 +1,26 @@
 package logic;
 
+import java.util.Collection;
+
 /**
  * Created by zorin on 23.01.2017.
  */
 public class ManageSystem {
+    private Collection<Group> groups;
+    private Collection<Student> students;
+    private static ManageSystem instance;
 
+    private ManageSystem () {
+
+    }
+
+    public static ManageSystem getInstance() {
+        if (instance == null)
+            instance = new ManageSystem();
+        return instance;
+    }
+
+    public static void main (String[] args) {
+
+    }
 }
